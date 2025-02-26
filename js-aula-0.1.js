45c04171d152c28870751f383d6191e7ab0dd062
AnandaMazine <anandacristine1@gmail.com>
2025-02-26 20:29:00 -0300

    @AnandaMazine

--- js-aula-0.1.js
+++ js-aula-0.1.js
@@ -19,3 +19,73 @@ cidade = "Jacupiranga" //let permite alterar o valor da variável
 
 const message = "Hello, World! Iniciando estudos com JavaScript!"
 console.log(message)
+// Typeof (esse comando exibirá o tipo da variável)
+const estado = "SP"
+const idade = 18
+let endereco
+console.log(typeof(estado))
+console.log(typeof(idade))
+console.log(typeof(endereco))
+
+//O JS é uma linguagem de tipagem dinâmica e tipagem fraca
+//////////////////////////////////////////////////////////
+
+// Tipos de funções no JS
+
+function minhaFuncao (){
+
+}
+console.log(typeof(minhaFuncao))
+
+// Função Simples
+function saudacao(){
+    console.log("Olá, bem-vindo!")
+}
+saudacao()
+
+// Função com parâmetros/argumento 
+// Parâmetro é um dado que a função recebe
+// Argumento é um dado que é enviado para a função
+
+function Saudacao(nome){ // PARÂMETRO
+    console.log("Olá, bem-vindo " + nome) // Concatenando
+    // Template Strings
+    console.log(`Olá, bem-vindo ${nome}`)
+    // $ {} - Placeholder
+    }
+Saudacao('Ananda') // ARGUMENTO
+
+// Função com mais de um parâmetro
+
+function soma(n1, n2){
+    let resultado = n1 + n2
+    console.log(`A soma dos dois numeros: ${resultado}`)
+}
+let n1 = 6
+let n2 = 10
+soma(n1, n2)
+
+//Função com retorno
+function Soma(n1, n2){
+    return n1 + n2
+}
+console.log(`A soma dos dois numeros foi ${Soma (2,6)}`)
+
+// Função com mais de um retorno
+function parImpar(n){
+    if (n % 2 == 0){
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+let num = 4
+console.log(parImpar(num))
+console.log(`O numero ${num} é ${parImpar(num)}!`)
+
+// Função Anônima
+let dobro = function(x){
+    return x*2
+}
+console.log(dobro(15))
+console.log(`O dobro do numero é ${dobro (15)}!`)
